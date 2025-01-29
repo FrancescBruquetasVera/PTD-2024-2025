f79cebe3d61326bdd12c7a5fec3b3244f472a538
FrancescBruquetasVera <152491246+FrancescBruquetasVera@users.noreply.github.com>
2025-01-29 13:38:18 +0100

    Add files via upload

---
+++ WWW 24-25/JAVA+RESPONSIVE/Treball 1/js/script.js
@@ -0,0 +1,30 @@
+function mostrarlogo(){
+    let info = document.getElementById("information");
+    let log = document.getElementById("logo");
+
+    if (info.style.display == "none") {
+      info.style.display = "block";
+      log.style.display = "none";
+    } else {
+      info.style.display = "none";
+      log.style.display = "block";
+    }
+
+  }
+  function mostrarlogo2(){
+    let log = document.getElementById("logo");
+
+    if (log.style.display == "none") {
+      
+      log.style.display = "block";
+    } else {
+      
+      log.style.display = "none";
+    }
+
+
+
+  }
+
+   
+
